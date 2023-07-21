6f6287d23e22824a7b08181ec25b6b7b16336f05
theshadsolo <rtmorgan83@gmail.com>
2023-07-21 13:07:54 -0400

    Challenge Change 1:   Created new table called Address   Adjusted User table with new FK to address id   Added two new rows in address   Associated rows in user with rows in address

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,13 +1,39 @@
 create table User
 (
     id        bigint              not null primary key,
+    addressId bigint,
     firstName varchar(255)        not null,
     lastName  varchar(255)        not null,
     username  varchar(255) unique not null,
     password  varchar(255)        not null -- WHAT!? NOT ENCRYPTED!? ;-)
 );
 
+create table Address
+(
+    id        bigint              not null primary key auto_increment,
+    address1  varchar(255)        not null,
+    address2  varchar(255),
+    city      varchar(255)        not null,
+    state     varchar(100)        not null,
+    postal    varchar(10)        not null
+);
+
+alter table User
+  add foreign key (addressId) references Address(id);
+
 insert into User
     (id, firstName, lastName, username, password)
 values (1, 'Phil', 'Ingwell', 'PhilIngwell', 'Password123') ,
     (2, 'Anna', 'Conda', 'AnnaConda', 'Password234');
+
+insert into Address (id, address1, city, state, postal)
+values (1, '1234 Park St.', 'Brooklyn', 'New York', '12345'),
+       (2, '5678 Main St.', 'Detroit', 'Michigan', '67890');
+
+update User
+  set addressId = 2
+  where id = 1;
+
+update User
+  set addressId = 1
+  where ID = 2;
